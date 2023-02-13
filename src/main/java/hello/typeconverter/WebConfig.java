92ccc2b52ea96daaca093fc440a88d7336eff08d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-13 21:25:33 +0900

    refactor: 포멧터 등록

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.typeconverter;
 
-import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
-import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.fomatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        //주석처리 우선순위
+//        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
         registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
