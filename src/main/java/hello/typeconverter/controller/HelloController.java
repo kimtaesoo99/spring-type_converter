b584f8241b843d2ddcf8592b4997c640084d84da
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-13 20:31:57 +0900

    feat: IpPort convert 컨트롤러

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +23,13 @@ public class HelloController {
         System.out.println("data = " + data);
         return "ok";
     }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort Ip= " + ipPort.getIp());
+        System.out.println("ipPort Port= " + ipPort.getPort());
+        return "ok";
+    }
+
+
 }
