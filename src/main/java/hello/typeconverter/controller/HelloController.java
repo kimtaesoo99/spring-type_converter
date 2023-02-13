58a717cb2326e7e6cf6e3744d288456e8f5c92b4
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-13 20:10:11 +0900

    refactor: controller패키지로 이동

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,4 +1,4 @@
-package hello.typeconverter;
+package hello.typeconverter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
